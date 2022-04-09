f8c98f337ec5a7d6db61bbeed367c0976f93544b
Wxin96 <18706306187@163.com>
2022-04-09 19:58:24 +0800

    220409

--- topic2/src/main/java/com/apollo/test5/Main04.java
+++ topic2/src/main/java/com/apollo/test5/Main04.java
@@ -1,7 +1,22 @@
 package com.apollo.test5;
 
+import java.util.Scanner;
+
 public class Main04 {
     public static void main(String[] args) {
-        
+        long s, t;
+        Scanner scanner = new Scanner(System.in);
+
+        s = scanner.nextLong();
+
+        while (s > 0) {
+            if (s % 2 == 0) {
+                s = s / 10;
+            } else {
+                t = s % 10;
+                System.out.print(t);
+                s = s / 10;
+            }
+        }
     }
 }
--- topic2/src/test/java/com/apollo/test/Test2.java
+++ topic2/src/test/java/com/apollo/test/Test2.java
@@ -3,6 +3,8 @@ package com.apollo.test;
 
 import org.junit.Test;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Test2 {
@@ -12,5 +14,40 @@ public class Test2 {
         stack.contains('a');
         StringBuilder str = new StringBuilder();
         str.insert(1, 'a');
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("123");
+        long s, t;
+        Scanner scanner = new Scanner(System.in);
+
+        s = scanner.nextLong();
+
+        while (s > 0) {
+            if (s % 2 == 0) {
+                s = s / 10;
+            } else {
+                t = s % 10;
+                System.out.println(t);
+                s = s / 10;
+            }
+        }
+
+    }
+
+
+    @Test
+    public void testJavaJob() {
+        long i = 0;
+        while (true) {
+            System.out.println(i++);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
